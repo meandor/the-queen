1195e49dbde30b6781c750238a3cac749e9215e8
Daniel Schruhl <danielschruhl@gmail.com>
2018-03-13 15:48:03 +0100

    add authentication for status and metering endpoint

--- src/com/github/meandor/core.clj
+++ src/com/github/meandor/core.clj
@@ -4,13 +4,21 @@
             [de.otto.goo.goo :as goo]
             [iapetos.collector.jvm :as jvm]
             [de.otto.tesla.serving-with-httpkit :as httpkit]
-            [de.otto.tesla.system :as system])
+            [de.otto.tesla.system :as system]
+            [de.otto.tesla.stateful.app-status :as app-status]
+            [de.otto.tesla.stateful.metering :as metering])
   (:gen-class))
+
+(defn- authenticated? [config user password]
+  (and (= user (get-in config [:authentication :username]))
+       (= password (get-in config [:authentication :password]))))
 
 (defn the-queen-system [runtime-config]
   (-> (system/base-system runtime-config)
-      ; (assoc)
-      (httpkit/add-server)))
+      (assoc
+        :app-status (c/using (app-status/new-app-status authenticated?) [:config :handler])
+        :metering (c/using (metering/new-metering authenticated?) [:config :handler]))
+      (httpkit/add-server :app-status :metering)))
 
 (defonce _ (jvm/initialize (goo/snapshot)))
 (defonce _ (Thread/setDefaultUncaughtExceptionHandler
