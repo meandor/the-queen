2a57babcfa028dfb3df01b115315f9f9bcb16a10
Daniel Schruhl <danielschruhl@gmail.com>
2018-03-13 11:23:46 +0100

    add tesla-microservice structure

--- project.clj
+++ project.clj
@@ -4,12 +4,20 @@
   :license {:name "Apache Version 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :scm {:name "git"
-        :url  "https://github.com/meandor/the-queen"}
+        :url  "https://github.com/meandor/the-queen.git"}
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.github.meandor/gimme "0.1.1"]
-                 [com.stuartsierra/component "0.3.2"]]
+                 [de.otto/tesla-microservice "0.11.25"]
+                 [de.otto/tesla-httpkit "1.0.1"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]]
+
+  :main ^:skip-aot com.github.meandor.core
+
+  :target-path "target/%s"
 
   :lein-release {:deploy-via :clojars}
-  :profiles {:dev {:plugins      [[lein-release/lein-release "1.0.9"]]
-                   :dependencies [[com.github.kstyrc/embedded-redis "0.6"]]}})
+  :profiles {:uberjar {:aot :all}
+             :dev     {:plugins      [[lein-release/lein-release "1.0.9"]]
+                       :dependencies [[com.github.kstyrc/embedded-redis "0.6"]]}})
---
+++ src/com/github/meandor/core.clj
@@ -0,0 +1,22 @@
+(ns com.github.meandor.core
+  (:gen-class)
+  (:require [clojure.tools.logging :as log]
+            [com.stuartsierra.component :as c]
+            [de.otto.goo.goo :as goo]
+            [iapetos.collector.jvm :as jvm]
+            [de.otto.tesla.serving-with-httpkit :as httpkit]
+            [de.otto.tesla.system :as system]))
+
+(defn the-queen-system [runtime-config]
+  (-> (system/base-system runtime-config)
+      ; (assoc)
+      (httpkit/add-server)))
+
+(defonce _ (jvm/initialize (goo/snapshot)))
+(defonce _ (Thread/setDefaultUncaughtExceptionHandler
+             (reify Thread$UncaughtExceptionHandler
+               (uncaughtException [_ thread ex]
+                 (log/error ex "Uncaught exception on " (.getName thread))))))
+
+(defn -main [& _]
+  (system/start (the-queen-system {})))
